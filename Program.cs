02cc8110c6ace6c200c00032e324df7298180a44
Joao Roas <jr.roas@gmail.com>
2022-06-01 11:52:15 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Orientacao_objetos_usando_um_jogo_RPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
